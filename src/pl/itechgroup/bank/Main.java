8cbbe1399ea6f9a6b55a9daddfff3faab3e4fd21
Tomasz Strzelecki <tomasz@strzelecki.website>
2021-10-25 11:07:12 +0200

    Excercise 3.1 solution

--- src/pl/itechgroup/bank/Main.java
+++ src/pl/itechgroup/bank/Main.java
@@ -3,6 +3,44 @@ package pl.itechgroup.bank;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // primitive types
+        boolean isNiceWeatherOutside = true;
+        System.out.println(isNiceWeatherOutside);
+        boolean isLightOn = false;
+        System.out.println(isLightOn);
+        char c = '@';
+        System.out.println(c);
+        char cTwo = '\u0040'; // Znak @
+        System.out.println(cTwo);
+        byte b = 15;
+        System.out.println(b);
+        int i = -1234;
+        System.out.println(i);
+        long len = 12345L;
+        System.out.println(len);
+        int bin = 0b101;
+        System.out.println(bin);
+        int octal = 077;
+        System.out.println(octal);
+        int hex = 0xFF;
+        System.out.println(hex);
+        long phoneNumber = 502_123_456;
+        System.out.println(phoneNumber);
+        float f = 10.1F;
+        System.out.println(f);
+        double big = 6.41e37;
+        System.out.println(big);
+        double transferAmmount = 12_000.6;
+        System.out.println(transferAmmount);
+        long bigTwo = 10;
+        System.out.println(bigTwo);
+        int val = 'a';
+        System.out.println(val);
+        float dec = 100;
+        System.out.println(dec);
+        int a = (int) 10L;
+        System.out.println(a);
+        float fTwo = (float) 10.0;
+        System.out.println(fTwo);
     }
 }
