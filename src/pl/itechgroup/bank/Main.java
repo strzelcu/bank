23d1c57ccb29751f423d110cc7a03e83416e7f49
Tomasz Strzelecki <tomasz@strzelecki.website>
2021-10-26 09:01:30 +0200

    Dni tygodnia switch

--- src/pl/itechgroup/bank/Main.java
+++ src/pl/itechgroup/bank/Main.java
@@ -2,22 +2,41 @@ package pl.itechgroup.bank;
 
 public class Main {
 
+    public enum DniTygodnia {
+        PN("PN", "Poniedziałek", 1),
+        WT("WT", "Wtorek", 2),
+        SR("SR", "Środa", 3),
+        CZ("CZ", "Czwartek", 4),
+        PT("PT", "Piątek", 5),
+        SB("SB", "Sobota", 6),
+        ND("ND", "Niedziela", 7);
+
+        String code;
+        String name;
+        int number;
+
+        DniTygodnia(String code, String name, int number) {
+            this.code = code;
+            this.name = name;
+            this.number = number;
+        }
+
+    }
+
     public static void main(String[] args) {
-        //        showOddNumbersWhile();
-        showOddNumbersFor();
-    }
 
-    static void showOddNumbersFor() {
-        for (int i = 1; i < 1000; i += 2) {
-            System.out.println(i);
+        for (DniTygodnia dzien:
+             DniTygodnia.values()) {
+            System.out.println(dzien.name);
+            switch (dzien.number) {
+                case 6:
+                case 7:
+                    System.out.println("Weekend!");
+                    break;
+                default:
+                    System.out.println("Niestety musisz jeszcze popracować");
+            }
         }
-    }
 
-    static void showOddNumbersWhile() {
-        int i = 1;
-        while (i < 1000) {
-            System.out.println(i);
-            i += 2;
-        }
     }
 }
